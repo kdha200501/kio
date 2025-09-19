c7eb186375e5b1b26ee435658346a431526b95bd
Jacks Diao <kdha200501@gmail.com>
2025-09-19 12:19:34 -0400

    jacks-customizations: one focus at a time

--- src/filewidgets/kdiroperator.cpp
+++ src/filewidgets/kdiroperator.cpp
@@ -1855,6 +1855,9 @@ void KDirOperator::setDirLister(KDirLister *lister)
     });
     connect(d->m_dirLister, qOverload<>(&KCoreDirLister::completed), this, [this]() {
         d->slotIOFinished();
+        if(view()) {
+            view()->setFocus();
+        }
     });
     connect(d->m_dirLister, qOverload<>(&KCoreDirLister::canceled), this, [this]() {
         d->slotCanceled();
